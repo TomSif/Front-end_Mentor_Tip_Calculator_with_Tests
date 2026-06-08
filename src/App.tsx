9a08e8f2ce854835abcadcd31e5b86b737ab0386
TomSif <thomas.sifferle.programmer@gmail.com>
2026-06-08 09:01:52 +0200

    chore(setup): initialize project with Vite, React, TypeScript, Tailwind CSS, clsx, tailwind-merge, Vitest, and React Testing Library

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="flex min-h-svh items-center justify-center bg-grey-200">
+      <h1 className="text-preset-1 text-green-900">Tip calculator app</h1>
+    </main>
+  )
+}
+
+export default App
