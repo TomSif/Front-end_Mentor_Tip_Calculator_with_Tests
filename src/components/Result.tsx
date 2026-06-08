489865d46c1bb9a46ae32d6dac9ec79288484561
TomSif <thomas.sifferle.programmer@gmail.com>
2026-06-08 12:26:47 +0200

    feat(result): create reusable Result component

---
+++ src/components/Result.tsx
@@ -0,0 +1,24 @@
+interface ResultProps {
+  label: string
+  result: number
+}
+
+const Result = ({ label, result }: ResultProps) => {
+  return (
+    <div className="flex w-full items-center justify-between">
+      <div className="flex flex-col gap-0.5">
+        <p className="text-preset-5 text-white">{label}</p>
+        <span className="text-grey-400 text-preset-6">/ person</span>
+      </div>
+      <div
+        role="status"
+        aria-live="polite"
+        className="text-preset-2 text-right text-green-400"
+      >
+        ${result ? result : '0.00'}
+      </div>
+    </div>
+  )
+}
+
+export default Result
