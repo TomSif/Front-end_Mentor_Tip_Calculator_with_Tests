e9d602687da6f65bf91b2cb5db7c9b36a72bfa1b
TomSif <thomas.sifferle.programmer@gmail.com>
2026-06-09 13:15:46 +0200

    feat(label): add error handling

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,13 +1,23 @@
 interface LabelProps {
   label: string
   id: string
+  isError: boolean
 }
 
-const Label = ({ label, id }: LabelProps) => {
+const Label = ({ label, id, isError }: LabelProps) => {
   return (
-    <label htmlFor={id} className="text-preset-5 text-grey-500">
-      {label}
-    </label>
+    <div className="relative w-full">
+      <label htmlFor={id} className="text-preset-5 text-grey-500 w-full">
+        {label}
+      </label>
+      {isError ? (
+        <p className="text-preset-5 absolute top-1/2 -right-12 -translate-1/2 text-right text-orange-400">
+          Can't be zero
+        </p>
+      ) : (
+        ''
+      )}
+    </div>
   )
 }
 
