c1ca2d15ee1a427f129310dd3fd1b53a6bce35a8
TomSif <thomas.sifferle.programmer@gmail.com>
2026-06-09 13:16:51 +0200

    feat(input): add onClick handler and active state

--- src/components/TipButton.tsx
+++ src/components/TipButton.tsx
@@ -1,10 +1,21 @@
+import { cn } from '../lib/utils'
+
 interface TipButtonProps {
   percent: number
+  isActive: boolean
+  onClick: (percent: number) => void
 }
 
-const TipButton = ({ percent }: TipButtonProps) => {
+const TipButton = ({ percent, isActive, onClick }: TipButtonProps) => {
   return (
-    <button className="text-preset-3 text-grey-50 w-full rounded-md bg-green-900 px-4 py-2 text-center hover:bg-green-200 hover:text-green-900 active:bg-green-400 active:text-green-900">
+    <button
+      type="button"
+      onClick={() => onClick(percent)}
+      className={cn(
+        'text-preset-3! text-grey-50 w-full rounded-md bg-green-900 px-4 py-2 text-center hover:bg-green-200 hover:text-green-900',
+        isActive ? 'bg-green-400 text-green-900' : '',
+      )}
+    >
       {percent}%
     </button>
   )
