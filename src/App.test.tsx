fbc9ffbffb6b6e60b516b5ac7da4687a7a296cfe
TomSif <thomas.sifferle.programmer@gmail.com>
2026-06-10 10:52:25 +0200

    test(app): add rendering smoke tests and enable RTL cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,27 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { describe, expect, it } from 'vitest'
 import App from './App'
 
 describe('App', () => {
-  it('renders the heading', () => {
+  it('renders the Bill', () => {
     render(<App />)
 
-    expect(
-      screen.getByRole('heading', { name: /tip calculator app/i }),
-    ).toBeInTheDocument()
+    expect(screen.getByLabelText('Bill')).toBeInTheDocument()
+  })
+  it('renders the button 10%', () => {
+    render(<App />)
+
+    expect(screen.getByRole('button', { name: '10%' })).toBeInTheDocument()
+  })
+  it('renders the input number of people', () => {
+    render(<App />)
+
+    expect(screen.getByLabelText('Number of People')).toBeInTheDocument()
+  })
+  it('renders the reset button', () => {
+    render(<App />)
+
+    expect(screen.getByRole('button', { name: 'RESET' })).toBeInTheDocument()
   })
 })
