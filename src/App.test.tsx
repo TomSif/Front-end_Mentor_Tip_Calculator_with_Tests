dd6780fb01381060bd8b3af90383a93731c9f3f2
TomSif <thomas.sifferle.programmer@gmail.com>
2026-06-13 09:02:01 +0200

    test(app): add happy path interaction test for tip calculation

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -24,4 +24,26 @@ describe('App', () => {
 
     expect(screen.getByRole('button', { name: 'RESET' })).toBeInTheDocument()
   })
+  it('remplir les inputs et clicker sur un bouton de pourcentage', async () => {
+    // Arrange : on rend le composant et on prépare l'utilisateur simulé
+    render(<App />)
+    const user = userEvent.setup()
+
+    // Arrange : on récupère les éléments avec lesquels on va interagir
+    const billInput = screen.getByLabelText('Bill')
+    const numberOfPeopleInput = screen.getByLabelText('Number of People')
+    const buttonTenPercent = screen.getByRole('button', { name: '10%' })
+    // Act : on simule ce que ferait un utilisateur, étape par étape
+    await user.type(billInput, '100')
+    await user.type(numberOfPeopleInput, '2')
+    await user.click(buttonTenPercent)
+
+    // Assert : viendra après, une fois qu'on sait quoi vérifier
+    expect(screen.getByRole('status', { name: 'Total' })).toHaveTextContent(
+      '$55.00',
+    )
+    expect(
+      screen.getByRole('status', { name: 'Tip Amount' }),
+    ).toHaveTextContent('$5.00')
+  })
 })
